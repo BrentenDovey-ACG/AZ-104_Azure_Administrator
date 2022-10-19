4f03340cb2320df7c536cee68d9a3ae12044a48b
BrentenDovey-ACG <brenten.dovey@acloud.guru>
2022-10-19 09:03:07 -0500

    Modify cleanerScript.ps1

--- cleanerScript copy.ps1
+++
@@ -1,5 +0,0 @@
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/BrentenDovey-ACG/AZ-104_Azure_Administrator/master/removeAll.json" -OutFile ~/removeAll.json
-
-$rg = az group list --query [].name -o tsv
-az deployment group create --resource-group $rg --mode Complete --template-file ~/removeAll.json
-
--- cleanerScript.ps1
+++ cleanerScript.ps1
@@ -1,24 +1,5 @@
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/BrentenDovey-ACG/AZ-104_Azure_Administrator/master/removeAll.json" -OutFile ~/removeAll.json
 
-# DO NOT RUN IN PRODUCTION ###################################################################################################################################
-# WARNING: This block of code is extremely destructive and will delete all resource(s) forcefully excluding only the storage account configured for cloudshell
-Get-AzResource | ForEach-Object -Parallel {
-    # Set variables
-    $resource = $_
-    $resourceName = $resource.Name
-    $resourceId = $resource.ResourceId
-    $cloudShellStorage = (Get-CloudDrive).StorageAccountName
+$rg = az group list --query [].name -o tsv
+az deployment group create --resource-group $rg --mode Complete --template-file ~/removeAll.json
 
-    # If/Else Logic to avoid deleting cloudsh
-    if ($resourceName -eq $cloudShellStorage) {
-        "Skipping... $resourceName"
-    }
-    else {
-        try {
-            "Deleting... $resourceName"
-            Remove-AzResource -Id $resourceId -Force | Out-Null
-        }
-        catch {
-            "Failed to Delete... $resourceName"
-        }
-    }
-}
